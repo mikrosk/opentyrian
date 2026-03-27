05004907a8b15cf07d39c48932e5061b97b7d46f
Carl Reinke <carlreinke@users.noreply.github.com>
2026-03-26 21:52:05 -0600

    Fix memory corruption due to too many data cubes
    The script for episode 3 specifies 5 data cubes before Savara Y, but the game
    only expects up to 4.

--- src/mainint.c
+++ src/mainint.c
@@ -2738,8 +2738,8 @@ void JE_endLevelAni(void)
 
 		if (cubeMax > 0)
 		{
-			if (cubeMax > 4)
-				cubeMax = 4;
+			if (cubeMax > COUNTOF(cubeList))
+				cubeMax = COUNTOF(cubeList);
 
 			if (frameCountMax != 0)
 				frameCountMax = 1;
